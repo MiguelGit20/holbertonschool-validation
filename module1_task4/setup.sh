77e444881660f798a7d66973b04389dd996a6985
MiguelGit20 <miguel.torres.20@hotmail.com>
2023-01-12 18:00:17 -0500

    setup update

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y hugo make
-wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
-apt install ./hugo_0.84.0_Linux-64bit.deb
+apt install curl
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt install -y ./hugo.deb
 make build
