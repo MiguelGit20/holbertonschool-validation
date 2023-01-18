813c75eaac40e3125232228bfef385b25b0ba901
MiguelGit20 <miguel.torres.20@hotmail.com>
2023-01-18 12:38:19 -0500

    update setup

--- module3_task3/setup.sh
+++ module3_task3/setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-apt update
-apt install -y git
-apt install -y hugo
-apt update
-apt-get update && apt-get install make
-apt install -y curl
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-apt install ./hugo.deb
+apt-get update && apt-get install -y curl make zip
+npm install -g markdownlint-cli
+
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo_extended.deb
+apt-get install -y ./hugo_extended.deb
+rm hugo_extended.deb
